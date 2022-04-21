c34ec53a911666de8d8714b48f6548ba2efea893
Muhammad Shahzad <shazee257@gmail.com>
2022-04-21 05:07:17 +0500

    MERN Added

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['Node JS', 'UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['Node JS', 'React JS', 'MERN', 'Mobile App', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
